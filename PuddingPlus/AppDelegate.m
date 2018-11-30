d13ebce1f3ead33c5786c04fe3a10a93d52621c1
samyoung79 <samyoung79@samyoung79ui-macbook-pro.local>
2018-11-30 16:34:02 +0900

    신규 커밋

--- PuddingPlus/AppDelegate.m
+++ PuddingPlus/AppDelegate.m
@@ -32,22 +32,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     [[RBProjectConfig getInstanse] loadConfig];
 
